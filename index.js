2302037d1c8d22ae31d23911157df3766aa7fe69
kry008 <pkrzysaaaa@gmail.com>
2022-01-31 10:36:05 +0100

    Start DiscordJS

---
+++ index.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env.DISCORD_TOKEN);
