b2f6325809530ac8e3867e321188fcea7c072403
kry008 <pkrzysaaaa@gmail.com>
2022-02-02 08:07:44 +0100

    Testowanie komend

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 var { prefix } = require('./config.json');
 const { Client, Intents } = require('discord.js');
+var fs = require('fs');
+
 
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"], partials: ["CHANNEL"]  });
 
@@ -28,20 +30,29 @@ client.on('messageCreate', async message => {
 		var lenpref = prefix.length
         const args = message.content.slice(lenpref).trim().split(/ +/);
         const commandName = args.shift().toLowerCase();
-		if (commandName == "add") {
-			
+		if (commandName == "add") 
+		{
+			fs.appendFile('dataBase.file', `${message.channel.id},`, function (err) {
+				if (err) throw err;
+			  });
 		}
 		else if(commandName == "del")
 		{
-
+			var all = "";
+			fs.readFile('dataBase.file', function(err, data) {
+				all = data
+				
+				var ids = all.split(",")
+				console.log(all);
+			})
 		}
 		else if (commandName == "status") 
 		{
-			
 		}
 
 		client.channels.cache.get("896383202023858187").send("Ok")
 		//console.log(client.channels.cache)
+			console.log(all);
 	}
 })
 client.login(process.env.DISCORD_TOKEN);
